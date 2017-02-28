9f1b84c1af6499fb01b5cc4899cf4cd1a2b28ee8
jorgegs29 <jorgegomaso@gmail.com>
2017-02-28 20:17:57 +0100

    Modelos

--- foro/app/Message.php
+++ foro/app/Message.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $table = 'Messaje';
+    public $timestamps = false;
+
+    public function thread() {
+        return $this->belonsTo('App\Thread');
+    }
+
 }
