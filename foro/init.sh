aa53da1c86449ec3735fe9f54bfef30353f560c0
vladern <vk18@alu.ua.es>
2017-04-07 13:29:36 +0200

    añado seeds

--- foro/init.sh
+++ foro/init.sh
@@ -4,6 +4,7 @@ composer install
 cp .env.example .env
 touch database/database.sqlite
 php artisan migrate
+php artisan db:seed
 wget https://phar.phpunit.de/phpunit-5.7.phar
 mv phpunit-5.7.phar phpunit.phar
 composer require "laravelcollective/html":"^5.4.0"
